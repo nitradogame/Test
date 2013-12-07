1ec39f82246bfda6e3e21c11a91602e1d8896e63
timmer2002 <anmelden.com@hotmail.de>
2013-12-08 00:04:01 +0100

    update

--- src/de/timmer/test/KillCommand.java
+++ src/de/timmer/test/KillCommand.java
@@ -1,14 +1,27 @@
 package de.timmer.test;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class KillCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender cs, Command cmd, String lable, String[] args) {
-		
+		if(cs instanceof Player){
+			
+			Player player = (Player) cs;
+			
+			if(args.length == 0) {
+				player.setHealth(0.0);
+				player.sendMessage("§4du warst so doff und hast dich selbst gekillt");
+			}else if(args.length == 1){
+				Player xd = Bukkit.getPlayer(args[0]); 
+				xd.setHealth(0.0);
+			}
+		}
 		return false;
 	}
 
--- src/de/timmer/test/Timmer.java
+++ src/de/timmer/test/Timmer.java
@@ -2,10 +2,11 @@ package de.timmer.test;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Timmer extends JavaPlugin{
+public class Timmer extends JavaPlugin {
 	
 	@Override
 	public void onEnable(){
 		this.getCommand("kill").setExecutor(new KillCommand());
 	}
+
 }
