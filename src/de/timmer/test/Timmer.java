7c4cc97c8c916d9f2f2f86fa7a62e580c66506aa
scholzi100 <scholzi_paul@me.com>
2013-12-08 00:19:21 +0100

    abc

--- src/de/timmer/test/Timmer.java
+++ src/de/timmer/test/Timmer.java
@@ -1,5 +1,6 @@
 package de.timmer.test;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Timmer extends JavaPlugin {
@@ -7,6 +8,7 @@ public class Timmer extends JavaPlugin {
 	@Override
 	public void onEnable(){
 		this.getCommand("kill").setExecutor(new KillCommand());
+		Bukkit.getPluginManager().registerEvents(new JoinQuitListener(), this);
 	}
 
 }
